81a11843defe9da53d703d48cd9e36a73d0f4565
mxmlm0681 <max-81mail.ru@mail.ru>
2025-01-28 20:27:32 +0300

    add changes

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,17 +1,10 @@
 import Character from "../app";
-
-test('Удачное создание зомби по имени Шон', () => {
-  const recieved = new Character('Shaun', 'Zombie');
-  const expected = {
-    name: 'Shaun',
-    type: 'Zombie',
-    level: 1,
-    health: 100,
-    attack: 40,
-    defence: 10,
-  };
-  expect(recieved).toEqual(expected);
-});
+import Boweman from "../Boweman";
+import Deamon from "../deamon";
+import Magician from "../magician";
+import Swordsman from "../swordsman";
+import Undead from "../undead";
+import Zombie from "../zombie";
 
 test('Имя не является строкой', () => {
   expect(() => new Character(7, 'Deamon')).toThrow();
@@ -27,4 +20,82 @@ test('Слишком длинное имя', () => {
 
 test('Неверный тип персонажа', () => {
   expect(() => new Character('Goblin', 'Dwarf')).toThrow();
+});
+
+test('Create Bowman', () => {
+  const character = new Boweman('Artur');
+  const expected = {
+    name: 'Artur',
+    level: 1,
+    health: 100,
+    type: 'Boweman',
+    attack: 25,
+    defence: 25,
+  };
+  expect(character).toEqual(expected);
+});
+
+test('Create Deamon', () => {
+  const character = new Deamon('Khaal');
+  const expected = {
+    name: 'Khaal',
+    level: 1,
+    health: 100,
+    type: 'Deamon',
+    attack: 10,
+    defence: 40,
+  };
+  expect(character).toEqual(expected);
+});
+
+test('Create Magician', () => {
+  const character = new Magician('Merlin');
+  const expected = {
+    name: 'Merlin',
+    level: 1,
+    health: 100,
+    type: 'Magician',
+    attack: 10,
+    defence: 40,
+  };
+  expect(character).toEqual(expected);
+});
+
+test('Create Swordsman', () => {
+  const character = new Swordsman('Aragorn');
+  const expected = {
+    name: 'Aragorn',
+    level: 1,
+    health: 100,
+    type: 'Swordsman',
+    attack: 40,
+    defence: 10,
+  };
+  expect(character).toEqual(expected);
+});
+
+test('Create Undead', () => {
+  const character = new Undead('Century');
+  const expected = {
+    name: 'Century',
+    level: 1,
+    health: 100,
+    type: 'Undead',
+    attack: 25,
+    defence: 25,
+  };
+  expect(character).toEqual(expected);
+});
+
+test('Create Zombie', () => {
+  const character = new Zombie('Nobady');
+  const expected = {
+    name: 'Nobady',
+    level: 1,
+    health: 100,
+    type: 'Zombie',
+    attack: 40,
+    defence: 10,
+  };
+  expect(character).toEqual(expected);
 });
